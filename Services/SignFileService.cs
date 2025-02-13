0f0068559babdabdfbe87d499ff6cfa3593a91b1
Vũ Đình Quang <quangvu@mxv.vn>
2025-02-13 16:52:51 +0700

    Add project files.

---
+++ Services/SignFileService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+
+namespace GrpcServiceSigner.Services
+{
+    public class SignFileService : SignFile.SignFileBase
+    {
+        public override Task<SignRes> Sign(SignReq req, ServerCallContext context)
+        {
+            Console.WriteLine(req);
+            return Task.FromResult(new SignRes
+            {
+                Success = true,
+                Txn = "asdkasdk",
+                Msg = "sdsda"
+            });
+        }
+    }
+}
